18b60638aee9b8ddf56c6165855b3c90e6a01d00
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-01 18:36:16 -0600

    feat: set grid state

--- grid/grid.go
+++ grid/grid.go
@@ -1,5 +1,7 @@
 package grid
 
+import "log"
+
 // Grid defines a grid and it's dimensions
 type Grid struct {
 	Width  int
@@ -15,4 +17,30 @@ func NewGrid(width, height int) *Grid {
 		cells[i] = make([]bool, width)
 	}
 	return &Grid{width, height, cells}
+}
+
+// SetState sets the cells state for the grid
+func (g *Grid) SetState(state [][]bool) {
+	if len(state) != g.Height || len(state[0]) != g.Width {
+		log.Fatalf("Given State (%d, %d) does not match Grid size (%d, %d)",
+			len(state), len(state[0]), g.Height, g.Width)
+	}
+	g.Cells = state
+}
+
+// dirs describes all 8 directions
+var dirs = [][]int{
+	// Top
+	{1, 1},
+	{1, -1},
+	{1, 0},
+
+	// Middle
+	{0, -1},
+	{0, 1},
+
+	// Bottom
+	{-1, 1},
+	{-1, -1},
+	{-1, 0},
 }
