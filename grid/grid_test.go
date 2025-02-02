a73cc55fc53295b9afef56a87ef8b8aa65fd6624
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-02-01 19:29:10 -0600

    test: live neighbor retrieval

--- grid/grid_test.go
+++ grid/grid_test.go
@@ -51,3 +51,31 @@ func TestSetState(t *testing.T) {
 		}
 	})
 }
+
+func TestGetNeighbors(t *testing.T) {
+	grid := NewGrid(3, 3)
+	state := [][]bool{
+		{true, false, false},
+		{true, false, true},
+		{false, false, true},
+	}
+	grid.SetState(state)
+
+	t.Run("center of grid", func(t *testing.T) {
+		got := grid.GetNeighbors(1, 1)
+		want := 4
+
+		if got != want {
+			t.Errorf("got %d live neighbors, want %d", got, want)
+		}
+	})
+
+	t.Run("boundary of grid", func(t *testing.T) {
+		got := grid.GetNeighbors(0, 0)
+		want := 1
+
+		if got != want {
+			t.Errorf("got %d live neighbors, want %d", got, want)
+		}
+	})
+}
